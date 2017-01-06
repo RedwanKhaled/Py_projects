f04065b23280e2ad8300cf1f9ad186bb824c5a14
RedwanKhaled <mohammad.islam@heavywater.solutions>
2017-01-05 20:00:14 -0500

    Recommendations projects...

---
+++ recommendations.py
@@ -0,0 +1,16 @@
+# A dictionary of movie critics and their ratings of a small
+# set of movies
+
+critics = {'Lisa Rose':{'Lady in the Water':2.5, 'Snakes on a plane': 3.5, 'Just My Luck': 3.0, 'Superman Returns': 3.5,
+                        'You, Me and Dupree': 2.5, 'The Night Listener': 3.0},
+            'Gene Saymour':{'Lady in the Water': 3.0, 'Snakes on a plane': 3.5, 'Just My Luck': 1.5, 'Superman Returns': 5.0,
+                            'You, Me and Dupree': 3.5, 'The Night Listener': 3.0},
+            'Michael Philips':{'Lady in the Water':2.5, 'Snakes on a plane': 3.0, 'Just My Luck': 3.0, 'Superman Returns': 3.5,
+                                'The Night Listener': 4.0},
+            'Claudia Puig':{'Snakes on a plane': 3.5, 'Just My Luck': 3.0, 'Superman Returns': 4.0,
+                            'You, Me and Dupree': 2.5, 'The Night Listener': 4.5},
+            'Mick LaSalle':{'Snakes on a plane': 4.0, 'Just My Luck': 2.0, 'Superman Returns': 3.0,
+                            'You, Me and Dupree': 2.0, 'The Night Listener': 3.0,'Lady in the Water':3.0},
+            'Jack Mathews':{'Snakes on a plane': 4.0, 'Superman Returns': 5.0,
+                            'You, Me and Dupree': 3.5, 'The Night Listener': 3.0,'Lady in the Water':3.0},
+            'Toby':{'Snakes on a plane': 4.5,'You, Me and Dupree': 1.0,'Superman Returns': 4.0}}
